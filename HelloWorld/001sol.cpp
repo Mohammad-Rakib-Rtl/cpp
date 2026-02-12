a3f2c33b1018b40ccc743c3c3e2010bc39a3208c
Mohammad-Rakib-Rtl <rtzrakib347@gmail.com>
2026-02-13 00:54:06 +0800

    add full cpp

---
+++ HelloWorld/001sol.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello from Mikey!" << endl;
+    cout << "" << endl;
+
+    cout << "   /|\\" << endl;
+    cout << "  / | \\" << endl;
+    cout << " /  |  \\" << endl;
+    cout << "/___|___\\" << endl;
+    
+
+    return 0;
+}
---
+++ sloppygeek/Untitled4.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "   /|" << endl;
+    cout << "  / |" << endl;
+    cout << " /  |" << endl;
+    cout << "/___|" << endl;
+    cout << "/___|" << endl;
+    cout << "Hello from mikey !" << endl;
+
+    return 0;
+}
