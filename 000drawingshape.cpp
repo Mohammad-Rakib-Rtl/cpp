f2b19ba5e6268cbf5f5fd0e3cdcefb20f3697311
Mohammad-Rakib-Rtl <rtzrakib347@gmail.com>
2026-02-14 04:37:26 +0800

    feat: add variables

---
+++ 000drawingshape.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello from Mikey!" << endl;
+    cout << "" << endl;
+
+    cout << "   /|\\" << endl;
+    cout << "  / | \\" << endl;
+    cout << " /  |  \\" << endl;
+    cout << "/___|___\\" << endl;
+    cout << "/___|___\\" << endl;
+    cout << "/___|___\\" << endl;
+
+
+    return 0;
+}
---
+++ 001variables.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string characterName = "Takemichi";
+    int characterAge,notCharAge;
+    characterAge = 26;
+    notCharAge = 30;
+
+    cout << "There once was a man named " << characterName << "!" << endl;
+    cout << "He was " << characterAge << " years old" << endl;
+    cout << "He liked the name " << characterName << endl;
+    cout << "But did not like being " << notCharAge << endl;
+
+
+    return 0;
+}
