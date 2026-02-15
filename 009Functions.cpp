6a1440a3564d606d752da2bc6d5bce06a782734b
Mohammad-Rakib-Rtl <rtzrakib347@gmail.com>
2026-02-15 20:08:23 +0800

    feat :D  functions

---
+++ 009Functions.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+
+void sayHi(){
+
+    cout << "Hello User";
+
+}
+
+
+int main() {
+
+    sayHi();
+    
+    return 0;
+}
