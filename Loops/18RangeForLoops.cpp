39d4d824352000576830b3f4915834c59ec450e0
Mohammad-Rakib-Rtl <rtzrakib347@gmail.com>
2026-02-21 20:03:51 +0800

    feat: add new code

---
+++ Loops/18RangeForLoops.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+
+    // int numbers[] = {1, 2, 3, 4};
+
+    // for (int i = 0; i < sizeof(numbers) / sizeof(int); i++)
+    //     cout << numbers[i] << endl;
+
+    // for (int number: numbers)
+    //     cout << number << endl;
+
+
+    // string name = "Mohammad Ratul";
+    // for (char ch: name)
+    //     cout << ch << endl;
+
+    /*
+        excercise
+
+        #Given a list of temperatures, 
+        write code to calculate the average temperature.
+
+    */
+
+    
+    
+
+
+
+
+
+    return 0;
+}
