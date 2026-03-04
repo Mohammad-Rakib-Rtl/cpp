a570b8e43b0ef4f909e7654cf0db74faabbf38e9
Mohammad-Rakib-Rtl <rtzrakib347@gmail.com>
2026-03-04 09:21:23 +0800

    pointers

---
+++ Pointers/0001declaringpointers.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+    // int number = 10;
+    // cout << &number;
+    
+    // //The address-of operator
+    // int* ptr = &number;
+
+    // //Indirection (de-referencing) operator
+    // *ptr = 20;
+    // cout << number;
+
+
+    //excercise for the lesson
+
+
+
+    int x = 10;
+    int y = 20;
+    
+    int* ptr = &x;
+    *ptr *= 2;
+    cout << "----------------" << endl;
+    cout << "X section" << endl;
+    cout << "----------------" << endl;
+    cout << x << endl;
+    cout << ptr << endl;
+    cout << *ptr << endl;
+
+    cout << "----------------" << endl;
+    cout << "Y section" << endl;
+    cout << "----------------" << endl;
+    ptr = &y;
+    *ptr *= 3;
+    cout << y << endl;
+    cout << ptr << endl;
+    cout << *ptr << endl;
+
+
+    return 0;
+}
